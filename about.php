a1f85e05ca29cadd50df9e8296f1ae46cdc4e91a
Aleksandra Petrukova <aleksandrapetrukova@gmail.com>
2023-06-05 21:13:24 -0400

    added files
    Created index.php, about.php, assets - images and .css

---
+++ about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header-flex">
+                <div class="logo">sasha.com</div>
+                <nav class="nav-bar">
+                    <a href="about.php">More about me</a>
+                    <a href="index.php">Main</a>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <h1>What I like</h1>
+        <div class="container">
+            <div class="like-flex">
+                <div class="card">
+                    <img src="assets/images/cat.jpeg" alt="">
+                    <h3>CATS</h3>
+                </div>
+                <div class="card">
+                    <img src="assets/images/catandhorse.jpeg" alt="">
+                    <h3>HORSES</h3>
+                </div>
+                <div class="card">
+                    <img src="assets/images/nap.jpeg" alt="">
+                    <h3>NAP</h3>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aleksandra - webdev env project</title>
+
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header-flex">
+                <div class="logo">sasha.com</div>
+                <nav class="nav-bar">
+                    <a href="about.php">More about me</a>
+                    <a href="index.php">Main</a>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <h1>About me</h1>
+        <div class="about-flex">
+            <img src="assets/images/img.jpeg" alt="">
+            <div class="right">
+                <p>
+                    Hello! My name is Aleksandra Petrukova, and I'm a passionate web developer based in Montreal City. I have a deep love for coding and creating visually appealing websites that provide a great user experience. I specialize in HTML, CSS, JavaScript, and various web frameworks.
+                </p>
+                <p>
+                    Feel free to contact me at <a href="mailto:2395214@edu.vaniercollege.qc.ca">2395214@edu.vaniercollege.qc.ca</a> for any inquiries or collaborations. I'm always excited to connect with fellow developers and work on exciting projects together.
+                </p>
+            </div>
+        </div>
+        
+    </main>
+</body>
+</html>
